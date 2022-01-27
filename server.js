4899b43dee179e586539aea2c555a75af40aae9f
“Lauren <lferrett@gmail.com>
2022-01-26 22:20:19 -0500

    Updated server js with session info.

--- server.js
+++ server.js
@@ -3,13 +3,31 @@ const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
 
+const routes = require('./controllers/');
+const session = require('express-session');
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const helpers = require('./utils/helpers');
 
 const hbs = exphbs.create({});
 
 // Sets up the Express App
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 
 // Describe what the following two lines of code are doing.
 // The following two lines of code are setting Handlebars.js as the default template engine.
@@ -19,10 +37,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(require('./controllers/'));
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log('Server listening on: http://localhost:' + PORT));
 });
-
-// Starts the server to begin listening
-app.listen(PORT, () => {
-    console.log('Server listening on: http://localhost:' + PORT);
-  });
