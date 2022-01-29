a89a19f54e923077c0fcab22e2087721229a8765
“Lauren <lferrett@gmail.com>
2022-01-28 23:53:20 -0500

    Rewrote post routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,25 +1,21 @@
-const router = require("express").Router();
-const { Post } = require("../../models");
-const withAuth = require("../../utils/auth");
+const router = require('express').Router();
+const { Post } = require('../../models/');
+const withAuth = require('../../utils/auth');
 
-router.post("/", withAuth, async (req, res) => {
-  const postBody = req.body;
+router.post('/', withAuth, async (req, res) => {
+  const body = req.body;
+    console.log(body);
   try {
-    const newPost = await Post.create({
-      ...body,
-      title: req.body.title,
-      post_content: req.body.post_content,
-      user_id: req.session.userId,
-    });
-    res.status(200).json(newPost);
-  } catch (err) {
-    console.log(err);
+    const newPost = await Post.create({ ...body, userId: req.session.userId });
+    res.json(newPost);
+     } catch (err) {
     res.status(500).json(err);
   }
 });
 
 router.put('/:id', withAuth, async (req, res) => {
   try {
+    console.log('here is the req.body', req.body);
     const [affectedRows] = await Post.update(req.body, {
       where: {
         id: req.params.id,
@@ -36,17 +32,19 @@ router.put('/:id', withAuth, async (req, res) => {
   }
 });
 
-router.delete("/:id", withAuth, async (req, res) => {
+router.delete('/:id', withAuth, async (req, res) => {
   try {
-    const deletePost = await Post.destroy({
+    const [affectedRows] = Post.destroy({
       where: {
         id: req.params.id,
       },
     });
 
-    !deletePost
-      ? res.status(404).json({ message: "No such post!" })
-      : res.status(200).json(deletePost);
+    if (affectedRows > 0) {
+      res.status(200).end();
+    } else {
+      res.status(404).end();
+    }
   } catch (err) {
     res.status(500).json(err);
   }
